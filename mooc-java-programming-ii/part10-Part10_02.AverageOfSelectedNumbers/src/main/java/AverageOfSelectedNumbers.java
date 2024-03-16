95d996ec264b299658035cea9c35fb6bd8bac0b0
Christopher Michael Chandler <christopher.chandler@outlook.de>
2024-03-16 08:50:39 +0100

    Finish 10_02_02

--- mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -1,12 +1,59 @@
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class AverageOfSelectedNumbers {
 
+    public static void determineIfPositiveOrNegative(String number){
+
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // toteuta ohjelmasi tänne
+        List<String> inputs = new ArrayList<>();
+
+        System.out.println("Input numbers, type \"end\" to stop.");
+// reading inputs
+        while (true) {
+            String row = scanner.nextLine();
+            if (row.equals("end")) {
+                break;
+            }
+
+            determineIfPositiveOrNegative(row);
+            inputs.add(row);
+        }
+
+
+// working out the average
+        double positive = inputs.stream()
+                .mapToInt(s -> Integer.valueOf(s)).filter(number -> number >= 0 )
+                .average()
+                .getAsDouble();
+
+        double negative = inputs.stream()
+                .mapToInt(s -> Integer.valueOf(s)).filter(number -> number < 0 )
+                .average()
+                .getAsDouble();
+
+        System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+        String row = scanner.nextLine();
+
+        if (row.equals("n")){
+
+            Locale.setDefault(Locale.US);
+
+            System.out.println(String.format("Average of the negative numbers: %.2f", negative ));
+        } else {
+            System.out.println(String.format("Average of the positive numbers: %.2f",  positive));
+
+        }
+
+
+
+
 
     }
 }

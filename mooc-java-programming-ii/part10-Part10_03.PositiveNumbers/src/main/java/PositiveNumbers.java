0963ba203a4985f85ffc3ec33d66efef5b5e0f47
Christopher Michael Chandler <christopher.chandler@outlook.de>
2024-03-16 11:17:29 +0100

    Finish 10_01_03

--- mooc-java-programming-ii/part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ mooc-java-programming-ii/part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -1,13 +1,46 @@
 
+
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class PositiveNumbers {
+    public static List<Integer> positive(List<Integer> numbers) {
 
+        List<Integer> positive = new ArrayList<>();
+
+
+        for (Integer i: numbers){
+
+            if (i >= 0){
+                positive.add(i);
+            }
+        }
+        return positive;
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // test your method here
+        List<Integer> list = new ArrayList<>();
+
+        while (true) {
+            String row = scanner.nextLine();
+            if (row.equals("end")) {
+                break;
+            }
+
+
+            list.add(Integer.valueOf(row));
+        }
+
+
+        ArrayList<Integer> values = list.stream()
+                .filter(value -> value > 5)
+                .map(value -> value * 2)
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        positive(values);
 
     }
 

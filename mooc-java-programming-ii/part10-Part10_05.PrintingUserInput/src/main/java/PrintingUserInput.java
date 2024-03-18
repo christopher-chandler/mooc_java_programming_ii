89bba793be1a334f588c1fc3539fa36b109a1392
Christopher chandler <christopher.chandler@outlook.de>
2024-03-18 08:13:34 +0100

    Finish 10_01_06

--- mooc-java-programming-ii/part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ mooc-java-programming-ii/part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -1,11 +1,24 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PrintingUserInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<String> inputs = new ArrayList<>();
 
+// reading inputs
+        while (true) {
+            String row = scanner.nextLine();
+            if (row.equals("")) {
+                break;
+            }
+
+            inputs.add(row);
+
+    }
+        for (String s: inputs){System.out.println(s);}
     }
 }

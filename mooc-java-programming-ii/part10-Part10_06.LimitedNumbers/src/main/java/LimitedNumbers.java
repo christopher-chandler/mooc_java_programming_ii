d4e53eeed4a4d4f1dfbc968610188d3194cd206d
Christopher chandler <christopher.chandler@outlook.de>
2024-03-18 08:19:32 +0100

    Finish 10_01_7

--- mooc-java-programming-ii/part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ mooc-java-programming-ii/part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -1,11 +1,31 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LimitedNumbers {
 
     public static void main(String[] args) {
+        // We initialise the scanner and the list into which the input is read
         Scanner scanner = new Scanner(System.in);
+        List<Integer> inputs = new ArrayList<>();
 
+// reading inputs
+        while (true) {
+            String row = scanner.nextLine();
+            if (Integer.parseInt(row) <0) {
+                break;
+            }
+
+            inputs.add(Integer.valueOf(row));
+        }
+
+        for (int i: inputs){
+
+            if (i > 0 && i <6){
+                System.out.println(i);
+            }
+
+        }
     }
 }

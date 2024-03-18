5e5a32a9a6ab224615833c82e42acf3de1feb3e7
Christopher chandler <christopher.chandler@outlook.de>
2024-03-18 12:18:17 +0100

    Finish 10_01_11

--- mooc-java-programming-ii/part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
+++ mooc-java-programming-ii/part10-Part10_10.BooksFromFile/src/main/java/BooksFromFile.java
@@ -7,10 +7,28 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class BooksFromFile {
-    
+    public static List<Book> readBooks(String file) {
+
+        List<Book> bookList = new ArrayList<>();
+        try {
+            // reading the "presidents.txt" file line by line
+            Files.lines(Paths.get(file))
+                    // splitting the row into parts on the "," character
+                    .map(row -> row.split(","))
+                    // creating persons from the parts
+                    .map(row -> new Book(row[0],Integer.valueOf(row[1]),Integer.valueOf(row[2]),row[3]))
+                    // and finally add the persons to the list
+                    .forEach(book -> bookList.add(book));
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return bookList;
+    }
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // test your method here
+
+        System.out.println(readBooks("src/main/java/file.txt").toString());
+
 
     }
 

cb39b6cdd7f3dc36acf03eb26f082e351fd9653f
Christopher chandler <christopher.chandler@outlook.de>
2024-08-12 14:32:17 -0500

    Finish 10_15

--- mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/Checker.java
+++ mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/Checker.java
@@ -13,5 +13,40 @@ public class Checker {
         }
     }
 
+    public boolean allVowels(String string) {
+        if (string.matches("[aieou]*")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean timeOfDay(String string) {
+
+
+        String[] time = string.split(":");
+
+        if (time.length != 3){
+            return false;
+        }
+
+        String hour = time[0];
+        String minute = time[1];
+        String second = time[2];
+
+
+        if (
+                hour.matches("0[0-9]|1[0-9]|2[0-3]") // Matches hours from 00 to 23
+                        && minute.matches("0[0-9]|[1-5][0-9]") // Matches minutes from 00 to 59
+                        && second.matches("0[0-9]|[1-5][0-9]") // Matches seconds from 00 to 59
+        )
+        {
+
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 
 }
--- mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
+++ mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
@@ -7,6 +7,6 @@ public class MainProgram {
     public static void main(String[] args) {
         Checker checker = new Checker();
 
-        System.out.println(checker.isDayOfWeek("tue"));
+        System.out.println(checker.timeOfDay("20:00:00"));
     }
 }

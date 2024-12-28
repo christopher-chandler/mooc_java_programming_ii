a2d9dabd5e82213b0585eab2b76b8deed834e42b
christopherchandler <christopher.chandler@outlook.de>
2024-12-28 12:50:21 +0100

    Finish 11_1_6

---
+++ mooc-java-programming-ii/part11-Part11_06.SaveablePerson/src/main/java/Person.java
@@ -0,0 +1,27 @@
+public class Person implements Saveable {
+
+    private String name;
+    private String address;
+
+public Person(String name, String address){
+    this.name = name;
+    this.address = address;
+
+}
+
+    @Override
+    public void save() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+
+
+    @Override
+    public void load(String address) {
+
+    }
+}

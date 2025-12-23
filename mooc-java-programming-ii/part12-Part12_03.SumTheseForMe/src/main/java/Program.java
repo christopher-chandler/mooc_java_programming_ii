8cb371f96825f2886d6aa5fd60c7ca436e90a11e
Christopher chandler <christopher.chandler@outlook.de>
2025-12-23 05:44:13 +0100

    Finish 12_02_03

--- mooc-java-programming-ii/part12-Part12_03.SumTheseForMe/src/main/java/Program.java
+++ mooc-java-programming-ii/part12-Part12_03.SumTheseForMe/src/main/java/Program.java
@@ -1,11 +1,48 @@
-
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Program {
 
+    public static int sum(int[] array, int fromWhere, int toWhere, int smallest, int largest){
+
+        if (fromWhere < 0){
+            fromWhere = 0;
+        }
+
+        if (toWhere > array.length){
+            toWhere = array[array.length-1];
+        }
+        int[] subArray  = Arrays.copyOfRange(array, fromWhere,toWhere);
+
+
+        int arraySum = 0;
+
+        for (int i = 0; i < subArray.length; i++) {
+
+            int arrayElement = subArray[i];
+
+
+            if (arrayElement > smallest && arrayElement < largest){
+
+                arraySum+=arrayElement;
+            }
+
+        }
+
+        return arraySum;
+    }
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        // test your method here
+        int[] numbers = {3, -1, 8, 4};
+
+
+        System.out.println(sum(numbers, 0, 0, 0, 0));
+        System.out.println(sum(numbers, 0, 0, 0, 10));
+        System.out.println(sum(numbers, 0, 1, 0, 10));
+        System.out.println(sum(numbers, 0, 1, -10, 10));
+        System.out.println(sum(numbers, -1, 999, -10, 10));
+
+
+
 
     }
 
